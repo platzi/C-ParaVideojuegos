36b7e9439cc9c2ffb1f88769e044c03e7f0d3f03
Juan Gabriel <juangabriel@frogames.es>
2018-10-30 10:45:51 +0100

    Clase numero 15

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState{
+    menu,
+    inGame,
+    gameOver
+}
+
+
+public class GameManager : MonoBehaviour {
+
+    public GameState currentGameState = GameState.menu;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartGame(){
+
+    } 
+
+    public void GameOver(){
+
+    }
+
+    public void BackToMenu(){
+
+    }
+
+    private void SetGameState(GameState newGameSate){
+        if(newGameSate == GameState.menu){
+            //TODO: colocar la lógica del menú
+        } else if(newGameSate == GameState.inGame){
+            //TODO: hay que preparar la escena para jugar
+        }else if(newGameSate == GameState.gameOver){
+            //TODO: preparar el juego para el Game Over
+        }
+
+
+        this.currentGameState = newGameSate;
+    }
+
+
+}
