7c351e4417840b8213c4b8fecaadf8ea06840e94
Juan Gabriel <juangabriel@frogames.es>
2018-10-30 16:52:30 +0100

    Clase numero 34

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+
+    public static MenuManager sharedInstance;
+    public Canvas menuCanvas;
+
+    private void Awake()
+    {
+        if (sharedInstance == null)
+        {
+            sharedInstance = this;
+        }
+    }
+    public void ShowMainMenu()
+    {
+        menuCanvas.enabled = true;
+    }
+
+    public void HideMainMenu()
+    {
+        menuCanvas.enabled = false;
+    }
+
+    public void ExitGame()
+    {
+    #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+    #else
+        Application.Quit();
+    #endif
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
