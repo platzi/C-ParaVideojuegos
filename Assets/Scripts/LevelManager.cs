3fa3b4322dc49ef5f7084787d672ec8e593cf1a8
Juan Gabriel <juangabriel@frogames.es>
2018-10-30 15:49:21 +0100

    Clase numero 29

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -57,8 +57,9 @@ public class LevelManager : MonoBehaviour {
     }
 
     public void RemoveLevelBlock(){
-
-
+        LevelBlock oldBlock = currentLevelBlocks[0];
+        currentLevelBlocks.Remove(oldBlock);
+        Destroy(oldBlock.gameObject);
     }
 
     public void RemoveAllLevelBlocks(){
