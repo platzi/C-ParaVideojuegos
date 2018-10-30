3d06b998e6d38c1376b3fc3b7e04d62575de265a
Juan Gabriel <juangabriel@frogames.es>
2018-10-30 12:57:42 +0100

    Clase numero 25

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public static LevelManager sharedInstance;
+
+    public List<LevelBlock> allTheLevelBlocks = new List<LevelBlock>();
+
+    public List<LevelBlock> currentLevelBlocks = new List<LevelBlock>();
+
+    public Transform levelStartPosition;
+
+    void Awake()
+    {
+        if(sharedInstance == null){
+            sharedInstance = this;
+        }
+    }
+    // Use this for initialization
+    void Start () {
+        GenerateInitialBlocks();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void AddLevelBlock(){
+
+    }
+
+    public void RemoveLevelBlock(){
+
+
+    }
+
+    public void RemoveAllLevelBlocks(){
+
+    }
+
+    public void GenerateInitialBlocks(){
+        for (int i = 0; i < 2; i++){
+            AddLevelBlock();
+        }
+    }
+}
