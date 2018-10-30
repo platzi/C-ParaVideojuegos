d55b265124a677e2a8059111bfb49f23c60bfd93
Juan Gabriel <juangabriel@frogames.es>
2018-10-30 17:41:50 +0100

    Clase numero 37

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CollectableType{
+    healthPotion,
+    manaPotion, 
+    money
+}
+
+
+public class Collectable : MonoBehaviour {
+
+    public CollectableType type = CollectableType.money;
+
+    private SpriteRenderer sprite;
+    private CircleCollider2D itemCollider;
+
+    bool hasBeenCollected = false;
+
+    public int value = 1;
+
+    private void Awake()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        itemCollider = GetComponent<CircleCollider2D>();
+    }
+
+    void Show(){
+        sprite.enabled = true;
+        itemCollider.enabled = true;
+        hasBeenCollected = false;
+    }
+
+
+    void Hide(){
+        sprite.enabled = false;
+        itemCollider.enabled = false;
+    }
+
+    void Collect(){
+        Hide();
+        hasBeenCollected = true;
+
+        switch(this.type){
+            case CollectableType.money:
+                break;
+
+            case CollectableType.healthPotion:
+                break;
+
+            case CollectableType.manaPotion:
+                break;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player"){
+            Collect();
+        }
+    }
+}
