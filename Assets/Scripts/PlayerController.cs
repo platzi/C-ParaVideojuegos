a8d5e9a2ce804cec55669960a3680644759a5c2a
Juan Gabriel <juangabriel@frogames.es>
2018-10-30 11:23:09 +0100

    Clase numero 19

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -56,9 +56,12 @@ public class PlayerController : MonoBehaviour {
 
     void Jump()
     {
-        if (IsTouchingTheGround())
+        if (GameManager.sharedInstance.currentGameState == GameState.inGame)
         {
-            rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            if (IsTouchingTheGround())
+            {
+                rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            }
         }
     }
 
