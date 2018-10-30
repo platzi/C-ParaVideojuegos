fb5d064baf62fd5be650f45d79f384316bd5c2bb
Juan Gabriel <juangabriel@frogames.es>
2018-10-30 09:35:15 +0100

    Clase numero 10

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    //Variables del movimiento del personaje
+    public float jumpForce = 6f;
+    Rigidbody2D rigidBody;
+
+    public LayerMask groundMask;
+
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
+            Jump();
+        }
+
+
+
+        Debug.DrawRay(this.transform.position, Vector2.down * 1.5f, Color.red);
+	}
+
+    void Jump()
+    {
+        if (IsTouchingTheGround())
+        {
+            rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    //Nos indica si el personaje está o no tocando el suelo
+    bool IsTouchingTheGround(){
+        if(Physics2D.Raycast(this.transform.position,
+                             Vector2.down,
+                             1.5f, 
+                             groundMask)){
+            //TODO: programar lógica de contacto con el suelo
+            return true;
+        }else {
+            //TODO: programar lógica de no contacto
+            return false;
+        }
+    }
+
+}
