5b88b3d7b9815788b97383c9e36b9d8916ac1262
Juan Gabriel <juangabriel@frogames.es>
2018-10-30 17:22:15 +0100

    Clase numero 36

---
+++ Assets/Scripts/GameView.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameView : MonoBehaviour {
+
+    public Text coinsText, scoreText, maxScoreText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(GameManager.sharedInstance.currentGameState == GameState.inGame){
+            int coins = 0;
+            float score = 0;
+            float maxScore = 0;
+
+            coinsText.text = coins.ToString();
+            scoreText.text = "Score: " + score.ToString("f1");
+            maxScoreText.text = "MaxScore: " + maxScore.ToString("f1");
+        }
+	}
+}
